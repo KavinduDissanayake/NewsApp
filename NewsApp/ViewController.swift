afd3a5240bfa06cdb9e94ba61f03724f5ff2d51e
“KavinduDissanayake” <kcrdissanayake@outlook.com>
2023-08-07 20:38:27 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Kavindu Dissanayake on 2023-08-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
